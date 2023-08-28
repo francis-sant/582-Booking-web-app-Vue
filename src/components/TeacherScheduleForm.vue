87414530067655ed341f8ca91ce22a513f37149a
Francisco <2295460@edu.vaniercollege.qc.ca>
2023-08-27 21:38:39 -0400

    component separated

--- src/components/TeacherScheduleForm.vue
+++ src/components/TeacherScheduleForm.vue
@@ -11,46 +11,22 @@
 
     <label for="type">Modality: Online, Class</label>
     <input type="text" v-model="modality" required />
-
-    <div class="months" for="months">
-      <h2>Months:</h2>
-      <label v-for="(monthLabel, monthValue) in months" :key="monthValue">
-        <input type="checkbox" :value="monthLabel" v-model="selectedMonths" />
-        {{ monthLabel }}
-      </label>
-    </div>
-
-    <label for="frequency">Frequency:</label>
-    <select v-model="selectedFrequency" id="frequency">
-      <option value="once">Once</option>
-      <option value="twice">Twice</option>
-      <option value="daily">Daily</option>
-    </select>
-
-    <div
-      v-if="selectedFrequency === 'once' || selectedFrequency === 'twice'"
-      for="dayOfWeek"
-    >
-      <label>Dia da semana:</label>
-      <label v-for="(day, index) in daysOfWeek" :key="index" value="day">
-        <input type="checkbox" :value="day" v-model="selectedDays" />
-        {{ day }}
-      </label>
-    </div>
-    <div v-if="selectedFrequency === 'daily'" for="dayOfWeek">
-      <label>Dia da semana:</label>
-      <label v-for="(day, index) in daysOfWeek" :key="index" value="day">
-        <input type="checkbox" :value="day" v-model="selectedDays" />
-        {{ day }}
-      </label>
-    </div>
+    <fieldset>
+      <legend>Date and Time Selection</legend>
+      <div v-for="(dateTime, index) in dateTimes" :key="index">
+        <label>Date:</label>
+        <input type="date" v-model="dateTime.date" required />
+        <label>Start Time:</label>
+        <input type="time" v-model="dateTime.startTime" required />
+        <label>End Time:</label>
+        <input type="time" v-model="dateTime.endTime" required />
+        <button @click="removeDateTime(index)">Remove</button>
+      </div>
+      <button @click="addDateTime">Add Date and Time</button>
+    </fieldset>
 
     <label for="type">Duration - in minutes</label>
     <input type="text" v-model="duration" required />
-
-    <label for="time">Available Time:</label>
-    <input type="time" v-model="startTime" required /> até
-    <input type="time" v-model="endTime" required />
 
     <label for="price">Price</label>
     <input type="number" v-model="price" required />
@@ -83,40 +59,23 @@ export default {
     const instructor = ref("");
     const name = ref("");
     const type = ref("");
-    const modality = ref("");
-    const selectedFrequency = ref("");
-    const selectedDays = ref([]);
-    const startTime = ref("");
-    const endTime = ref("");
     const price = ref("");
+    const modality = ref("");
+    const selectedDays = ref([]);
     const successMessage = ref("");
     const failMessage = ref("");
-    const selectedMonths = ref([]);
     const duration = ref("");
 
-    const daysOfWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
+    const dateTimes = ref([
+      { date: "", startTime: "", endTime: "" },
+    ]);
 
-    const months = {
-      "01": "Janeiro",
-      "02": "Fevereiro",
-      "03": "Março",
-      "04": "Abril",
-      "05": "Maio",
-      "06": "Junho",
-      "07": "Julho",
-      "08": "Agosto",
-      "09": "Setembro",
-      10: "Outubro",
-      11: "Novembro",
-      12: "Dezembro",
+    const addDateTime = () => {
+      dateTimes.value.push({ date: "", startTime: "", endTime: "" });
+    };
+
+    const removeDateTime = (index) => {
+      dateTimes.value.splice(index, 1);
     };
 
     // const teacherStore = useTeacherStore();
@@ -128,13 +87,9 @@ export default {
         type: type.value,
         name: name.value,
         modality: modality.value,
-        frequency: selectedFrequency.value,
-        months: selectedMonths.value,
-        days: selectedDays.value,
-        startTime: startTime.value,
-        endTime: endTime.value,
         price: price.value,
         duration: duration.value,
+        dateTimes: dateTimes.value,
       };
 
       try {
@@ -160,11 +115,6 @@ export default {
       name.value = "";
       type.value = "";
       modality.value = "";
-      months.value = "";
-      selectedMonths.value = [];
-      selectedFrequency.value = "";
-      startTime.value = "";
-      endTime.value = "";
       price.value = "";
       selectedDays.value = [];
       duration.value = "";
@@ -175,19 +125,16 @@ export default {
       name,
       type,
       modality,
-      months,
-      selectedMonths,
-      selectedFrequency,
-      startTime,
-      endTime,
       price,
-      daysOfWeek,
       selectedDays,
       // isFormSubmitted,
       submitSchedule,
       successMessage,
       failMessage,
       duration,
+      dateTimes,
+      addDateTime,
+      removeDateTime,
     };
   },
 };
